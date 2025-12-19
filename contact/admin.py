01b9e0964a8dfef354f5e89e944dc67d2a21d9ef
Bruno Vieira <bvcomdigital@gmail.com>
2025-12-18 23:14:51 -0300

    Implementação de tabela com css e html

--- contact/admin.py
+++ contact/admin.py
@@ -3,13 +3,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id', #se colocar - na frente ordena decrescente
     #list_filter = 'created_date', -> pesquisa pela data de criação
     search_fields = 'id', 'first_name', 'last_name', #camps de pesquisa
     list_per_page = 10 #qts exibe na página
     list_max_show_all = 200 #max exibido na pagina, se não limitar ele mostra tudo
-    list_editable = 'first_name', 'last_name', #para editar campos
+    list_editable = 'first_name', 'last_name', 'show', #para editar campos
     list_display_links = 'id', 'phone', #cria um link nestes campos
     
 @admin.register(models.Category)
