9273ae30293291a313c2face1d51d5af018901aa
Bruno Vieira <bvcomdigital@gmail.com>
2025-12-20 11:58:36 -0300

    criou formulario dinamico

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q # Para colocar o ou nos filtros de busca
 from django.core.paginator import Paginator
+from django import forms
 from contact.models import Contact
 
-def create(request):
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+            )
 
+
+def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
 
     context = {
+        'form': ContactForm()
     }
 
     return render(
