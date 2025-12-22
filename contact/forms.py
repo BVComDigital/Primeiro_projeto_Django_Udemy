a390e7bcf11ff52631670b6a5cffb65eeec783ab
Bruno Vieira <bvcomdigital@gmail.com>
2025-12-22 16:57:40 -0300

    Criando widgets

--- contact/forms.py
+++ contact/forms.py
@@ -5,11 +5,42 @@ from . import  models
 
 # Cria o formulario de contatos
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class':  'classe-a',
+                'placeholder': 'Escreva aqui',
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda par ao usuário'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self. fields['first_name'].widget.attrs.update(
+        #     { 
+        #        'class':  'classe-a',
+        #        'placeholder': 'Escreva aqui', 
+        #     }
+        # )
+
+
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
             )
+        
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs = {
+        #             'class':  'classe-a',
+        #             'placeholder': 'Escreva aqui',
+        #         }
+        #     )
+        # }
         
     
     def clean(self):
