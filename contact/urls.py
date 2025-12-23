0b4df2e7ead74304e0b5410a928b9fa68f24a233
Bruno Vieira <bvcomdigital@gmail.com>
2025-12-23 16:49:18 -0300

    criou botões delete update

--- contact/urls.py
+++ contact/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     # contact (CRUD)
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),  
     path('contact/create/', views.create, name='create'),  
-    # path('contact/<int:contact_id>/update/', views.contact, name='contact'),  
-    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),  
+    path('contact/<int:contact_id>/update/', views.update, name='update'),  
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),  
 
     # users (CRUD)
     # path('user/<int:contact_id>/detail/', views.contact, name='contact'),  
