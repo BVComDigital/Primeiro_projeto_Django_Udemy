f4784af099361be1c9871367fd56a5182152feda
Bruno Vieira <bvcomdigital@gmail.com>
2025-12-24 18:35:47 -0300

    implementou mensagens do sistema

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from contact.forms import RegisterForm
 
 def register(request):
     form = RegisterForm()
 
+    # messages.info(request, 'Bem vindo ao sistema de registro.')
+    # messages.success(request, 'Sucesso no registro.')
+    # messages.warning(request, 'Atenção necessária.')
+    # messages.error(request, 'Erro no registro.')
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            form = RegisterForm()  # Reset the form after successful registration
+            messages.success(request, 'Sucesso no registro.')
+            return redirect('contact:index')
+          #  form = RegisterForm()   Reset the form after successful registration
 
 
     return render(request, 
