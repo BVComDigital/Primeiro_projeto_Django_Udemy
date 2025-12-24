664bdddee87e2fb217d430e59cfe7c41b960e7c3
Bruno Vieira <bvcomdigital@gmail.com>
2025-12-24 15:25:40 -0300

    criou campo imagem

--- contact/forms.py
+++ contact/forms.py
@@ -1,47 +1,26 @@
 from django import forms # Importa formularios do Django
 from django.core.exceptions import ValidationError
-from . import  models
+from contact import  models
 
 
 # Cria o formulario de contatos
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class':  'classe-a',
-                'placeholder': 'Escreva aqui',
-            }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda par ao usuário'
+    picture = forms.ImageField(
+         widget=forms.FileInput(
+              attrs={
+                   'accept': 'image/*',
+              }
+         )
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self. fields['first_name'].widget.attrs.update(
-        #     { 
-        #        'class':  'classe-a',
-        #        'placeholder': 'Escreva aqui', 
-        #     }
-        # )
-
 
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
+            
         )
-        
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs = {
-        #             'class':  'classe-a',
-        #             'placeholder': 'Escreva aqui',
-        #         }
-        #     )
-        # }
         
     
     def clean(self):
